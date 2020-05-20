2e2fced203071759797ff0f89f3fe374b0bfa5ca
Soulballad <soda931vzr@163.com>
2020-05-20 22:26:43 +0800

    change pom

--- usage-spring/spring-boot/spring-boot-04-form-validate/src/test/java/com/soulballad/usage/springboot/SpringBoot04FormValidateApplicationTests.java
+++ usage-spring/spring-boot/spring-boot-04-form-validate/src/test/java/com/soulballad/usage/springboot/SpringBoot04FormValidateApplicationTests.java
@@ -1,12 +1,10 @@
 package com.soulballad.usage.springboot;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBoot04FormValidateApplicationTests {
 
-    @Test
     void contextLoads() {}
 
 }
