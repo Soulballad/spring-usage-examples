e53400ab84985072f7255cf4e24a3536e1a51fd3
Soulballad <soda931vzr@163.com>
2020-05-24 22:29:16 +0800

    add

---
+++ usage-spring/spring-boot/spring-boot-08-template/spring-boot-template-freemarker/src/main/java/com/soulballad/usage/springboot/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springboot.repository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author ：soulballad
+ * @version : v1.0
+ * @apiNote : repository
+ * @since ：2020/5/24 22:25
+ */
+@Repository
+public class UserRepository {
+}
