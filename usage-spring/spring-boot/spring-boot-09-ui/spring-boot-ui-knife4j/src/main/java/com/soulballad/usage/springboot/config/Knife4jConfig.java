bdda6409e5850c1e46a0e8a01c11536fc564c274
Soulballad <soda931vzr@163.com>
2020-05-27 22:38:18 +0800

    add

--- usage-spring/spring-boot/spring-boot-09-ui/spring-boot-ui-knife4j/src/main/java/com/soulballad/usage/springboot/config/Knife4jConfig.java
+++ usage-spring/spring-boot/spring-boot-09-ui/spring-boot-ui-knife4j/src/main/java/com/soulballad/usage/springboot/config/Knife4jConfig.java
@@ -1,8 +1,10 @@
 package com.soulballad.usage.springboot.config;
 
-import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -20,7 +22,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-@EnableSwaggerBootstrapUI
+@EnableKnife4j
 public class Knife4jConfig {
 
     private ApiInfo apiInfo() {
