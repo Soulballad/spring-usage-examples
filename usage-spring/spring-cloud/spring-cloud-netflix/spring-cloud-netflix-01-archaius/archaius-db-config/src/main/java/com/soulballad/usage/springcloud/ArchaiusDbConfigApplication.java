bae37d86ad51eb7ba8082f8ab23ad63476108aeb
Soulballad <soda931vzr@163.com>
2020-05-31 17:09:47 +0800

    add

---
+++ usage-spring/spring-cloud/spring-cloud-netflix/spring-cloud-netflix-01-archaius/archaius-db-config/src/main/java/com/soulballad/usage/springcloud/ArchaiusDbConfigApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchaiusDbConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchaiusDbConfigApplication.class, args);
+    }
+
+}
