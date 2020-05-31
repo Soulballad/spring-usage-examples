0c6cab4d41e2510cf791184426ce8ad68ed72270
Soulballad <soda931vzr@163.com>
2020-05-31 17:16:18 +0800

    add

---
+++ usage-spring/spring-cloud/spring-cloud-netflix/spring-cloud-netflix-01-archaius/archaius-db-config/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into properties
+values('spring.cloud.netflix.archaius.db.one', 'one from db properties');
+
+insert into properties
+values('spring.cloud.netflix.archaius.db.two', 'two from db properties');
