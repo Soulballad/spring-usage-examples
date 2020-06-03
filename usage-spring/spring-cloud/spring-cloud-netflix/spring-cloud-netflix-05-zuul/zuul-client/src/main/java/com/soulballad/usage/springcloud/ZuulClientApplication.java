841322f4dea7122884bd1aea7837ddd5e5532e10
Soulballad <soda931vzr@163.com>
2020-06-03 20:11:56 +0800

    add

--- usage-spring/spring-cloud/spring-cloud-netflix/spring-cloud-netflix-05-zuul/zuul-client/src/main/java/com/soulballad/usage/springcloud/ZuulClientApplication.java
+++ usage-spring/spring-cloud/spring-cloud-netflix/spring-cloud-netflix-05-zuul/zuul-client/src/main/java/com/soulballad/usage/springcloud/ZuulClientApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCloudNetflix05ZuulApplication {
+public class ZuulClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudNetflix05ZuulApplication.class, args);
+        SpringApplication.run(ZuulClientApplication.class, args);
     }
 
 }
