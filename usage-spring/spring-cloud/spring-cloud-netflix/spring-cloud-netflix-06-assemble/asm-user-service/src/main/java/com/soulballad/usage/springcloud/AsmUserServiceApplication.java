d82df6a206612465dfb60b6ca1d89f4d041eb2a7
Soulballad <soda931vzr@163.com>
2020-06-04 22:07:46 +0800

    add

---
+++ usage-spring/spring-cloud/spring-cloud-netflix/spring-cloud-netflix-06-assemble/asm-user-service/src/main/java/com/soulballad/usage/springcloud/AsmUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsmUserServiceApplication.class, args);
+    }
+
+}
