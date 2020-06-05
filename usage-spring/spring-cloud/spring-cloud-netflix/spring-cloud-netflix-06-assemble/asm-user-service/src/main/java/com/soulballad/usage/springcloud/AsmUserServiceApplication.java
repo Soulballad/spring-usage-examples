5ddacd3a6eb369479dec99e3f02ed936cd782675
Soulballad <soda931vzr@163.com>
2020-06-05 21:05:20 +0800

    add

--- usage-spring/spring-cloud/spring-cloud-netflix/spring-cloud-netflix-06-assemble/asm-user-service/src/main/java/com/soulballad/usage/springcloud/AsmUserServiceApplication.java
+++ usage-spring/spring-cloud/spring-cloud-netflix/spring-cloud-netflix-06-assemble/asm-user-service/src/main/java/com/soulballad/usage/springcloud/AsmUserServiceApplication.java
@@ -3,8 +3,14 @@ package com.soulballad.usage.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+@EnableTurbine
+@EnableHystrix
 @EnableEurekaClient
+@EnableHystrixDashboard
 @SpringBootApplication
 public class AsmUserServiceApplication {
 
