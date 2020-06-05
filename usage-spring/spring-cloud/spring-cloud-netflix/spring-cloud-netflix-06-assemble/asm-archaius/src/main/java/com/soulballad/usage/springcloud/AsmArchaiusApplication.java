7a12778a0ef8c438ec6b20f1a92e00953081d78b
Soulballad <soda931vzr@163.com>
2020-06-05 20:09:18 +0800

    add

---
+++ usage-spring/spring-cloud/spring-cloud-netflix/spring-cloud-netflix-06-assemble/asm-archaius/src/main/java/com/soulballad/usage/springcloud/AsmArchaiusApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmArchaiusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsmArchaiusApplication.class, args);
+    }
+
+}
