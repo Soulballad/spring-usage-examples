990f404cd4151f87cd825dc2f7c9e54c71b8ec5d
Soulballad <soda931vzr@163.com>
2020-06-11 20:09:48 +0800

    add

---
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-01-config/src/main/java/com/soulballad/usage/springcloud/SpringCloudOfc01ConfigApplication.java
@@ -0,0 +1,12 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOfc01ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudOfc01ConfigApplication.class, args);
+    }
+}
