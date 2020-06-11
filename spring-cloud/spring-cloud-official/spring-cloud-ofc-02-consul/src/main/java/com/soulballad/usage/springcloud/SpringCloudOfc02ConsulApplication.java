0ce4aca61920caddef198a5127d7ef1bd64e3998
Soulballad <soda931vzr@163.com>
2020-06-11 21:58:57 +0800

    add

---
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-02-consul/src/main/java/com/soulballad/usage/springcloud/SpringCloudOfc02ConsulApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOfc02ConsulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudOfc02ConsulApplication.class, args);
+    }
+
+}
---
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-03-zookeeper/src/main/java/com/soulballad/usage/springcloud/SpringCloudOfc03ZookeeperApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOfc03ZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudOfc03ZookeeperApplication.class, args);
+    }
+
+}
