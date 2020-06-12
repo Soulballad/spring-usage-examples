8bf10a89cda83082cb9a15ea5c7c29fa985566ea
Soulballad <soda931vzr@163.com>
2020-06-12 22:10:02 +0800

    add

---
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-02-zookeeper/ofc-zk-registry-consumer/src/main/java/com/soulballad/usage/spirngcloud/OfcZkRegistryConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.soulballad.usage.spirngcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfcZkRegistryConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfcZkRegistryConsumerApplication.class, args);
+    }
+}
--- spring-cloud/spring-cloud-official/spring-cloud-ofc-02-zookeeper/ofc-zk-registry-provider/src/main/java/com/soulballad/usage/springcloud/OfcZkRegistryProviderApplication.java
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-02-zookeeper/ofc-zk-registry-provider/src/main/java/com/soulballad/usage/springcloud/OfcZkRegistryProviderApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCloudOfc03ZookeeperApplication {
+public class OfcZkRegistryProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudOfc03ZookeeperApplication.class, args);
+        SpringApplication.run(OfcZkRegistryProviderApplication.class, args);
     }
-
 }
