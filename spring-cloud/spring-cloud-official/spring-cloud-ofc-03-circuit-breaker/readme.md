35aec396c86db13576519d4a0387574425a2e91e
Soulballad <soda931vzr@163.com>
2020-06-14 15:49:19 +0800

    add

--- spring-cloud/spring-cloud-official/spring-cloud-ofc-03-circuit-breaker/readme.md
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-03-circuit-breaker/readme.md
@@ -1,5 +1,6 @@
 spring-cloud-circuit-breaker 有四种实现，分别是：
 - netflix-hystrix
 - Resilience4J
+    1.  resilience4j-SpringBoot2-demo
 - alibaba sentinel
 - spring retry
---
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-04-feign/ofc-feign-01-demo/ofc-user-api/src/main/java/com/soulballad/usage/springcloud/OfcUserApiApplication.java
@@ -0,0 +1,12 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfcUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfcUserApiApplication.class, args);
+	}
+}
