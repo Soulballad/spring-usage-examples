b4f7a9c06f55fbb4965e348337f99e973df6bde6
Soulballad <soda931vzr@163.com>
2020-06-14 18:29:54 +0800

    add

--- spring-cloud/spring-cloud-official/spring-cloud-ofc-05-gateway/ofc-gateway-router/src/main/java/com/soulballad/usage/springcloud/OfcGatewayRouterApplication.java
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-05-gateway/ofc-gateway-router/src/main/java/com/soulballad/usage/springcloud/OfcGatewayRouterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCloudOfc05GatewayApplication {
+public class OfcGatewayRouterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudOfc05GatewayApplication.class, args);
+        SpringApplication.run(OfcGatewayRouterApplication.class, args);
     }
 
 }
