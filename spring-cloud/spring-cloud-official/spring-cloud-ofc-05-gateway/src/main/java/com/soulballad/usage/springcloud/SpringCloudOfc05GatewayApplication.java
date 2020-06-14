49ae93d306b65781732dfad99dcda0c32ff0a2c3
Soulballad <soda931vzr@163.com>
2020-06-14 17:29:37 +0800

    add

---
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-05-gateway/src/main/java/com/soulballad/usage/springcloud/SpringCloudOfc05GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOfc05GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudOfc05GatewayApplication.class, args);
+    }
+
+}
