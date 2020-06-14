89ff6ce18065360908a357bff883d6c03ea0816e
Soulballad <soda931vzr@163.com>
2020-06-14 15:53:17 +0800

    add

---
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-04-feign/ofc-feign-01-demo/ofc-user-client/src/main/java/com/soulballad/usage/springcloud/OfcUserClientApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfcUserClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfcUserClientApplication.class, args);
+	}
+
+}
