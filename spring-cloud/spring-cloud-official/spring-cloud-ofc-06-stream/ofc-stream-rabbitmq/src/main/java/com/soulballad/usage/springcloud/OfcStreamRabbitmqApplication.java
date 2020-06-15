6292daf00a186a2f0ac17d745019cd4217e38972
Soulballad <soda931vzr@163.com>
2020-06-15 21:43:21 +0800

    add

---
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-06-stream/ofc-stream-rabbitmq/src/main/java/com/soulballad/usage/springcloud/OfcStreamRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfcStreamRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfcStreamRabbitmqApplication.class, args);
+    }
+
+}
