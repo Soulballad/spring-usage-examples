c230aa30994391432730dbbc112dca6ea9282c7d
Soulballad <soda931vzr@163.com>
2020-06-16 22:38:19 +0800

    add

--- spring-cloud/spring-cloud-official/spring-cloud-ofc-07-bus/ofc-bus-eureka/src/main/java/com/soulballad/usage/springcloud/OfcBusEurekaApplication.java
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-07-bus/ofc-bus-eureka/src/main/java/com/soulballad/usage/springcloud/OfcBusEurekaApplication.java
@@ -2,12 +2,13 @@ package com.soulballad.usage.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class SpringCloudOfc07BusApplication {
+public class OfcBusEurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudOfc07BusApplication.class, args);
+        SpringApplication.run(OfcBusEurekaApplication.class, args);
     }
-
 }
