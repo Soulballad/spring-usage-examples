eac6c6b832218246c26ff51c42f142dd5e7a0f1c
Soulballad <soda931vzr@163.com>
2020-06-16 22:26:31 +0800

    add

---
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-07-bus/src/test/java/com/soulballad/usage/springcloud/SpringCloudOfc07BusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.soulballad.usage.springcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudOfc07BusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
