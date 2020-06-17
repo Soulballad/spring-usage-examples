c6a53537cb9ad77ec5958a7994343e280115afd3
Soulballad <soda931vzr@163.com>
2020-06-17 20:54:58 +0800

    add

---
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-10-security/src/main/java/com/soulballad/usage/springcloud/SpringCloudOfc10SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOfc10SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudOfc10SecurityApplication.class, args);
+    }
+
+}
