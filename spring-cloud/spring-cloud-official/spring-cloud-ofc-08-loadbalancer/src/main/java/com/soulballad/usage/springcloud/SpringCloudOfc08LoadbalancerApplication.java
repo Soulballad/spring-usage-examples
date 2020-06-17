3c0a4b6952b61d6457411961d2b95e9d8417c5d8
Soulballad <soda931vzr@163.com>
2020-06-17 20:36:03 +0800

    add

---
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-08-loadbalancer/src/main/java/com/soulballad/usage/springcloud/SpringCloudOfc08LoadbalancerApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOfc08LoadbalancerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudOfc08LoadbalancerApplication.class, args);
+    }
+
+}
