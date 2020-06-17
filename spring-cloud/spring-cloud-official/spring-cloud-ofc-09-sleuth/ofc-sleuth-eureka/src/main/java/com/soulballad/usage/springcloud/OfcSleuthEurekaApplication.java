763f9f4cce5c1b3260fd479a450da6d98fe81fef
Soulballad <soda931vzr@163.com>
2020-06-17 21:00:51 +0800

    add

--- spring-cloud/spring-cloud-official/spring-cloud-ofc-09-sleuth/ofc-sleuth-eureka/src/main/java/com/soulballad/usage/springcloud/OfcSleuthEurekaApplication.java
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-09-sleuth/ofc-sleuth-eureka/src/main/java/com/soulballad/usage/springcloud/OfcSleuthEurekaApplication.java
@@ -2,12 +2,13 @@ package com.soulballad.usage.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class SpringCloudOfc09SleuthApplication {
+public class OfcSleuthEurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudOfc09SleuthApplication.class, args);
+        SpringApplication.run(OfcSleuthEurekaApplication.class, args);
     }
-
 }
