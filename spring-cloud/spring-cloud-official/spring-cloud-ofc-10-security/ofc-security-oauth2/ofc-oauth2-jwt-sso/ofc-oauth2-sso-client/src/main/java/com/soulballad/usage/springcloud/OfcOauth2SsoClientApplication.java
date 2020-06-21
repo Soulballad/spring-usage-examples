d7877be4739d974096c2439f8265330bcf197b83
Soulballad <soda931vzr@163.com>
2020-06-21 18:37:33 +0800

    add

--- spring-cloud/spring-cloud-official/spring-cloud-ofc-10-security/ofc-security-oauth2/ofc-oauth2-jwt-sso/ofc-oauth2-sso-client/src/main/java/com/soulballad/usage/springcloud/OfcOauth2SsoClientApplication.java
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-10-security/ofc-security-oauth2/ofc-oauth2-jwt-sso/ofc-oauth2-sso-client/src/main/java/com/soulballad/usage/springcloud/OfcOauth2SsoClientApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OfcOauth2JwtSsoApplication {
+public class OfcOauth2SsoClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OfcOauth2JwtSsoApplication.class, args);
+        SpringApplication.run(OfcOauth2SsoClientApplication.class, args);
     }
 
 }
---
+++ spring-cloud/spring-cloud-official/spring-cloud-ofc-10-security/ofc-security-oauth2/ofc-oauth2-jwt-sso/ofc-oauth2-sso-server/src/main/java/com/soulballad/usage/springcloud/OfcOauth2SsoServerApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfcOauth2SsoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfcOauth2SsoServerApplication.class, args);
+    }
+
+}
