526d1c1dc03b0ef29425960319d24c4245f920b9
Soulballad <soda931vzr@163.com>
2020-06-22 21:01:42 +0800

    add

---
+++ spring-cloud/spring-cloud-alibaba/spring-cloud-alb-01-nacos/alb-nacos-registry/src/main/java/com/soulballad/usage/springcloud/AlbNacosRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbNacosRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbNacosRegistryApplication.class, args);
+    }
+
+}
