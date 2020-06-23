ab12d4322ea9ef77f1abb29d684b351516ded2b5
Soulballad <soda931vzr@163.com>
2020-06-23 20:03:00 +0800

    add

---
+++ spring-cloud/spring-cloud-alibaba/spring-cloud-alb-01-nacos/alb-nacos-gateway/alb-gateway-router/src/main/java/com/soulballad/usage/springcloud/AlbGatewayRouterApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbGatewayRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbGatewayRouterApplication.class, args);
+    }
+
+}
