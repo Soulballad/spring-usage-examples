d703cdf9f7a3b6dc4b1aea749528083d0a58e18b
Soulballad <soda931vzr@163.com>
2020-06-25 22:50:25 +0800

    add

---
+++ spring-cloud/spring-cloud-alibaba/spring-cloud-alb-02-dubbo/alb-dubbo-gateway/src/main/java/com/soulballad/usage/springcloud/AlbDubboGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbDubboGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbDubboGatewayApplication.class, args);
+    }
+
+}
