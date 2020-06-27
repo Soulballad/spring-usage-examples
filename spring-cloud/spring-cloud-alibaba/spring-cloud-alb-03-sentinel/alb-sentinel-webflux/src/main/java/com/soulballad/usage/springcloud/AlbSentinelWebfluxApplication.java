e2b7dcbc3daeaa2ddfc5e294f5ac6e06cc20c947
Soulballad <soda931vzr@163.com>
2020-06-27 17:13:23 +0800

    add

---
+++ spring-cloud/spring-cloud-alibaba/spring-cloud-alb-03-sentinel/alb-sentinel-webflux/src/main/java/com/soulballad/usage/springcloud/AlbSentinelWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbSentinelWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbSentinelWebfluxApplication.class, args);
+    }
+
+}
