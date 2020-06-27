bf22172f5377d3384eea279ce3f96f45b00570f9
Soulballad <soda931vzr@163.com>
2020-06-27 15:17:05 +0800

    add

---
+++ spring-cloud/spring-cloud-alibaba/spring-cloud-alb-03-sentinel/src/test/java/com/soulballad/usage/springcloud/SpringCloudAlb03SentinelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.soulballad.usage.springcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudAlb03SentinelApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
