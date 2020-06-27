ab487884fdab2225d68f3e0ecc9e7fe00ef406ef
Soulballad <soda931vzr@163.com>
2020-06-27 17:25:22 +0800

    add

---
+++ spring-cloud/spring-cloud-alibaba/spring-cloud-alb-03-sentinel/alb-sentinel-integrate-feign/src/main/java/com/soulballad/usage/springcloud/AlbSentinelIntegrateFeignApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbSentinelIntegrateFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbSentinelIntegrateFeignApplication.class, args);
+    }
+
+}
