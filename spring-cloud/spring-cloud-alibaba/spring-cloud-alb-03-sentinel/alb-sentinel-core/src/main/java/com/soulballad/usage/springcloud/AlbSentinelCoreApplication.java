f04f4beb86e82a0d6fe78f132996487b95f5785d
Soulballad <soda931vzr@163.com>
2020-06-27 17:08:10 +0800

    add

--- spring-cloud/spring-cloud-alibaba/spring-cloud-alb-03-sentinel/alb-sentinel-core/src/main/java/com/soulballad/usage/springcloud/AlbSentinelCoreApplication.java
+++ spring-cloud/spring-cloud-alibaba/spring-cloud-alb-03-sentinel/alb-sentinel-core/src/main/java/com/soulballad/usage/springcloud/AlbSentinelCoreApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCloudAlb03SentinelApplication {
+public class AlbSentinelCoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudAlb03SentinelApplication.class, args);
+        SpringApplication.run(AlbSentinelCoreApplication.class, args);
     }
 
 }
