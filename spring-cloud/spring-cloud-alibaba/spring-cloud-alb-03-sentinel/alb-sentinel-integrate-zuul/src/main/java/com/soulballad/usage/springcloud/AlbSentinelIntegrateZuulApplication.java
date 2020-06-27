32fd66717c43f7f4057a016d0b1b7bc82f583a0f
Soulballad <soda931vzr@163.com>
2020-06-27 17:28:41 +0800

    add

---
+++ spring-cloud/spring-cloud-alibaba/spring-cloud-alb-03-sentinel/alb-sentinel-integrate-zuul/src/main/java/com/soulballad/usage/springcloud/AlbSentinelIntegrateZuulApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbSentinelIntegrateZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbSentinelIntegrateZuulApplication.class, args);
+    }
+
+}
