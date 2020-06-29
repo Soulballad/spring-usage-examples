b41d7bc8ef479371bfa363d57bf06e3c807e5ce7
Soulballad <soda931vzr@163.com>
2020-06-29 19:23:43 +0800

    add

--- spring-cloud/spring-cloud-alibaba/spring-cloud-alb-03-sentinel/alb-sentinel-integrate-gateway/src/main/java/com/soulballad/usage/springcloud/controller/RuleWebFluxController.java
+++ spring-cloud/spring-cloud-alibaba/spring-cloud-alb-03-sentinel/alb-sentinel-integrate-gateway/src/main/java/com/soulballad/usage/springcloud/controller/RuleWebFluxController.java
@@ -1,7 +1,17 @@
 package com.soulballad.usage.springcloud.controller;
 
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author ：soulballad
@@ -12,5 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RuleWebFluxController {
 
+    @GetMapping(value = "/api")
+    public Mono<Set<ApiDefinition>> apiRules() {
+        return Mono.just(GatewayApiDefinitionManager.getApiDefinitions());
+    }
 
+    @GetMapping(value = "/gateway")
+    public Mono<Set<GatewayFlowRule>> apiGateway() {
+        return Mono.just(GatewayRuleManager.getRules());
+    }
+
+    @GetMapping(value = "/flow")
+    public Mono<List<FlowRule>> apiFlow() {
+        return Mono.just(FlowRuleManager.getRules());
+    }
 }
