9a039a774cd9b64b8c29a72ac00a0e84ac248cd6
Soulballad <soda931vzr@163.com>
2020-06-29 19:36:29 +0800

    add

--- spring-cloud/spring-cloud-alibaba/spring-cloud-alb-03-sentinel/alb-sentinel-integrate-zuul/src/main/java/com/soulballad/usage/springcloud/config/SentinelZuulConfig.java
+++ spring-cloud/spring-cloud-alibaba/spring-cloud-alb-03-sentinel/alb-sentinel-integrate-zuul/src/main/java/com/soulballad/usage/springcloud/config/SentinelZuulConfig.java
@@ -1,6 +1,12 @@
 package com.soulballad.usage.springcloud.config;
 
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.callback.RequestOriginParser;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.fallback.BlockResponse;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.fallback.ZuulBlockFallbackProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author ：soulballad
@@ -10,4 +16,36 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class SentinelZuulConfig {
+
+    @Bean
+    public ZuulBlockFallbackProvider zuulBlockFallbackProvider() {
+        return new ZuulBlockFallbackProvider() {
+            @Override
+            public String getRoute() {
+                return "*";
+            }
+
+            @Override
+            public BlockResponse fallbackResponse(String route, Throwable cause) {
+                String msgPrefix = "alb-sentinel-integrate-zuul blocked ";
+                if ("my-service3".equals(route)) {
+                    return new BlockResponse(433, msgPrefix + "my-service3", route);
+                } else if ("my-service4".equals(route)) {
+                    return new BlockResponse(444, msgPrefix + "my-service4", route);
+                } else {
+                    return new BlockResponse(499, msgPrefix + "default 499", route);
+                }
+            }
+        };
+    }
+
+    @Bean
+    public RequestOriginParser requestOriginParser() {
+        return new RequestOriginParser() {
+            @Override
+            public String parseOrigin(HttpServletRequest request) {
+                return "123";
+            }
+        };
+    }
 }
--- spring-cloud/spring-cloud-alibaba/spring-cloud-alb-03-sentinel/alb-sentinel-integrate-zuul/src/main/java/com/soulballad/usage/springcloud/controller/SentinelZuulController.java
+++ spring-cloud/spring-cloud-alibaba/spring-cloud-alb-03-sentinel/alb-sentinel-integrate-zuul/src/main/java/com/soulballad/usage/springcloud/controller/SentinelZuulController.java
@@ -1,7 +1,16 @@
 package com.soulballad.usage.springcloud.controller;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author ：soulballad
@@ -9,7 +18,21 @@ import org.springframework.context.annotation.Configuration;
  * @apiNote : controller
  * @since ：2020/6/28 21:38
  */
-@Configuration
+@RestController
 public class SentinelZuulController {
 
+    @GetMapping("/api")
+    public Set<ApiDefinition> apiRules() {
+        return GatewayApiDefinitionManager.getApiDefinitions();
+    }
+
+    @GetMapping("/gateway")
+    public Set<GatewayFlowRule> apiGateway() {
+        return GatewayRuleManager.getRules();
+    }
+
+    @GetMapping("/flow")
+    public List<FlowRule> apiFlow() {
+        return FlowRuleManager.getRules();
+    }
 }
