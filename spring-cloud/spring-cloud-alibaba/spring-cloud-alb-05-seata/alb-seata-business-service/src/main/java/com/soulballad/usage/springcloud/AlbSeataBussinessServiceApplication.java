cf55fb727af68066a29bba7d527de7e58edc115d
Soulballad <soda931vzr@163.com>
2020-06-30 21:24:33 +0800

    add

---
+++ spring-cloud/spring-cloud-alibaba/spring-cloud-alb-05-seata/alb-seata-business-service/src/main/java/com/soulballad/usage/springcloud/AlbSeataBussinessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.soulballad.usage.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbSeataBussinessServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbSeataBussinessServiceApplication.class, args);
+    }
+
+}
