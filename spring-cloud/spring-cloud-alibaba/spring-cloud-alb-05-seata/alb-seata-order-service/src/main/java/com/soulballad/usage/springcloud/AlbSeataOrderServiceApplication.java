f9ee08040ceb52fdf79e1904bf529a437e22aa0b
Soulballad <soda931vzr@163.com>
2020-06-30 21:59:26 +0800

    add

--- spring-cloud/spring-cloud-alibaba/spring-cloud-alb-05-seata/alb-seata-order-service/src/main/java/com/soulballad/usage/springcloud/AlbSeataOrderServiceApplication.java
+++ spring-cloud/spring-cloud-alibaba/spring-cloud-alb-05-seata/alb-seata-order-service/src/main/java/com/soulballad/usage/springcloud/AlbSeataOrderServiceApplication.java
@@ -2,6 +2,8 @@ package com.soulballad.usage.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class AlbSeataOrderServiceApplication {
@@ -10,4 +12,8 @@ public class AlbSeataOrderServiceApplication {
         SpringApplication.run(AlbSeataOrderServiceApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
