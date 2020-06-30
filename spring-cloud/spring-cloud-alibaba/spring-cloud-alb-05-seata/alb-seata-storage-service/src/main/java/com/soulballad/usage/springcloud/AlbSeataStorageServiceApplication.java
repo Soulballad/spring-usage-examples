33d740ff200393da4e77dca04dee8641780bdb79
Soulballad <soda931vzr@163.com>
2020-06-30 22:04:09 +0800

    add

--- spring-cloud/spring-cloud-alibaba/spring-cloud-alb-05-seata/alb-seata-storage-service/src/main/java/com/soulballad/usage/springcloud/AlbSeataStorageServiceApplication.java
+++ spring-cloud/spring-cloud-alibaba/spring-cloud-alb-05-seata/alb-seata-storage-service/src/main/java/com/soulballad/usage/springcloud/AlbSeataStorageServiceApplication.java
@@ -9,5 +9,4 @@ public class AlbSeataStorageServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AlbSeataStorageServiceApplication.class, args);
     }
-
 }
--- spring-cloud/spring-cloud-alibaba/spring-cloud-alb-05-seata/alb-seata-storage-service/src/main/java/com/soulballad/usage/springcloud/seata/StorageController.java
+++ spring-cloud/spring-cloud-alibaba/spring-cloud-alb-05-seata/alb-seata-storage-service/src/main/java/com/soulballad/usage/springcloud/seata/StorageController.java
@@ -25,9 +25,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author xiaojing
- */
 @RestController
 public class StorageController {
 
