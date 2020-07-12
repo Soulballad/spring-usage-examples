9ce34e81352b36c3df118f51660a9639336b058a
Soulballad <soda931vzr@163.com>
2020-07-12 21:01:49 +0800

    add

---
+++ spring-boot/spring-boot-05-basis/spring-boot-profile/src/main/java/com/soulballad/usage/springboot/SpringBootProfileApplication.java
@@ -0,0 +1,12 @@
+package com.soulballad.usage.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProfileApplication.class, args);
+    }
+}
