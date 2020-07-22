484e1061f8bfd20222bf0772450e613aaee22010
Soulballad <soda931vzr@163.com>
2020-07-22 22:09:36 +0800

    add

--- spring-boot/spring-boot-06-jdbc/spring-boot-jdbc-template/src/main/java/com/soulballad/usage/springboot/SpringBootJdbcTemplateApplication.java
+++ spring-boot/spring-boot-06-jdbc/spring-boot-jdbc-template/src/main/java/com/soulballad/usage/springboot/SpringBootJdbcTemplateApplication.java
@@ -9,5 +9,4 @@ public class SpringBootJdbcTemplateApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJdbcTemplateApplication.class, args);
     }
-
 }
