cd0c28659ac21f3691ee226bd1183cd61b4c943e
Soulballad <soda931vzr@163.com>
2020-07-30 19:38:50 +0800

    add

--- spring-boot/spring-boot-11-custom-starter/test-spring-boot-starter/src/main/java/com/soulballad/usage/springboot/TestSpringBootStarterApplication.java
+++ spring-boot/spring-boot-11-custom-starter/test-spring-boot-starter/src/main/java/com/soulballad/usage/springboot/TestSpringBootStarterApplication.java
@@ -9,5 +9,4 @@ public class TestSpringBootStarterApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestSpringBootStarterApplication.class, args);
     }
-
 }
